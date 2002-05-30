81cba50f1f8731f4d5f2de6b0a52f6a160b518f4
Lori Corbani <Lori.Corbani@jax.org>
2002-05-30 18:52:34 +0000

    fix

--- annotload.py
+++ annotload.py
@@ -364,7 +364,7 @@ def verifyMode():
 				'and a._AnnotType_key = %s\n' % (annotTypeKey), None, execute = not DEBUG)
 			db.sql('delete VOC_Annot from VOC_Annot a ' + \
 				'where not exists (select 1 from VOC_Evidence e ' + \
-				'where a._Annot_key = e._Annot_key)', None, execute = not DEBUG)
+				'where a._Annot_key = e._Annot_key)\n', None, execute = not DEBUG)
 		else:
 			db.sql('delete VOC_Annot from VOC_Annot ' + \
 				'where _AnnotType_key = %s\n' % (annotTypeKey), None, execute = not DEBUG)
