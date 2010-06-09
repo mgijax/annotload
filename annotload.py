ebc02efb4cbfc982f9199f304d3cba29936db974
Lori Corbani <Lori.Corbani@jax.org>
2010-06-09 17:27:36 +0000

    TR10109

--- annotload.py
+++ annotload.py
@@ -832,7 +832,7 @@ def bcpFiles():
 	os.system(bcpNoteChunk)
 
 	# for GO/GAF annotations only...
-	if delByUser in ('GOA%', 'RGD', 'GOC'):
+	if delByUser in ('GOA%', 'RGD%', 'GOC%'):
 	    execSQL = 'exec VOC_deleteGOGAFRed "%s"' % (delByUser)
 	    print execSQL
 	    db.sql(execSQL, None)
